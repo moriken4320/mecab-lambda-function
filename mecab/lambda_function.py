a6801f06e8bc6f0bb19fd0184a77abb67c51114d
森田健太 <k_morita@arsaga.jp>
2023-02-19 15:24:03 +0900

    fixed: fix

--- mecab/lambda_function.py
+++ mecab/lambda_function.py
@@ -1,28 +1,22 @@
 import json
-import os
-import ctypes
-
-mecabdir = os.path.join(os.getcwd(), '/var/task/.mecab')
-libmecab = ctypes.cdll.LoadLibrary(os.path.join(mecabdir, 'lib/libmecab.so'))
-
 import MeCab
 
-output_format_type = 'chasen'
-dicdir = os.path.join(mecabdir, 'lib/mecab/dic/ipadic')
-rcfile = os.path.join(mecabdir, 'etc/mecabrc')
+tagger = MeCab.Tagger(
+    '-O wakati '
+    '-r /dev/null '
+    # '-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd'
+)
 
-tagger = MeCab.Tagger('-O{} -d{} -r{}'.format(output_format_type, dicdir, rcfile))
 
 def lambda_handler(event, context):
-    req_body = json.loads(event['body'])
-    res_body = {
-      'morphemes': tagger.parse(req_body['text'])
-    }
+    node = tagger.parseToNode(event['body'])
+
+    result = []
+    while node:
+        if not node.feature.startswith('BOS/EOS'):
+            result.append(node.feature)
+        node = node.next
+
     return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(res_body)
+        "body": json.dumps(result),
     }
