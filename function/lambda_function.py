bc251585d34e3c49b517dfa4214dc3754d50a625
森田健太 <k_morita@arsaga.jp>
2023-02-19 22:56:41 +0900

    fixed: replace

---
+++ function/lambda_function.py
@@ -0,0 +1,16 @@
+import MeCab
+import os
+import ctypes
+
+mecabdir = os.path.join(os.getcwd(), '/var/task/.mecab')
+libmecab = ctypes.cdll.LoadLibrary(os.path.join(mecabdir, 'lib/libmecab.so'))
+
+output_format_type = 'wakati'
+dicdir = os.path.join(mecabdir, 'lib/mecab/dic/ipadic')
+rcfile = os.path.join(mecabdir, 'etc/mecabrc')
+
+def lambda_handler(event, context):
+    sentence = event['sentence']
+    tagger = MeCab.Tagger('-O{} -d{} -r{}'.format(output_format_type, dicdir, rcfile))
+    parsed_sentence = tagger.parse(sentence)
+    return {'result': parsed_sentence}
