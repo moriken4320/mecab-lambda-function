9ff4d9bf7ce2e4048faef8ff225c9e1adaf31f3c
森田健太 <k_morita@arsaga.jp>
2023-02-19 10:57:07 +0900

    add: first commit

---
+++ mecab/lambda_function.py
@@ -0,0 +1,28 @@
+import json
+import os
+import ctypes
+
+mecabdir = os.path.join(os.getcwd(), '/var/task/.mecab')
+libmecab = ctypes.cdll.LoadLibrary(os.path.join(mecabdir, 'lib/libmecab.so'))
+
+import MeCab
+
+output_format_type = 'chasen'
+dicdir = os.path.join(mecabdir, 'lib/mecab/dic/ipadic')
+rcfile = os.path.join(mecabdir, 'etc/mecabrc')
+
+tagger = MeCab.Tagger('-O{} -d{} -r{}'.format(output_format_type, dicdir, rcfile))
+
+def lambda_handler(event, context):
+    req_body = json.loads(event['body'])
+    res_body = {
+      'morphemes': tagger.parse(req_body['text'])
+    }
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(res_body)
+    }
